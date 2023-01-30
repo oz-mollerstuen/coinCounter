1ee5e9cb9b521a6d37ba00f34446c0e884934d27
oz-mollerstuen <mollerstuen.works@gmail.com>
2023-01-30 10:16:00 -0800

    add recursive coding
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -16,24 +16,42 @@ function coinCounter(amount) {
   return change;
 }
 
-function coinCounterFunc(amount) {
-  const coinTypes = [25, 10, 5, 1]
-  const coinNames = ["quarters", "dimes", "nickels", "pennies"]
-  const total = amount * 100;
-  const output = {};
-  function getCoins(numIndex, left) {
+// function coinCounterFunc(amount) {
+//   const coinTypes = [25, 10, 5, 1]
+//   const coinNames = ["quarters", "dimes", "nickels", "pennies"]
+//   const total = amount * 100;
+//   const output = {};
+//   function getCoins(numIndex, left) {
+//     const coinType = Math.floor(left / coinTypes[numIndex]);
+//     const coinOut = left % coinTypes[numIndex];
+//     output[coinNames[numIndex]] = coinType;
+//     if (numIndex < coinTypes.length - 1) {
+//       getCoins(numIndex + 1, coinOut);
+//     }
+//   }
+//   getCoins(0, total);
+//   return (output);
+// }
+
+function coinCounterFunc(coinTypes, coinNames, numIndex, left, output) {
     const coinType = Math.floor(left / coinTypes[numIndex]);
     const coinOut = left % coinTypes[numIndex];
-    output[coinNames[numIndex]] = coinType;
-    if (numIndex < coinTypes.length - 1) {
-      getCoins(numIndex + 1, coinOut);
+    const name = coinNames[numIndex];
+    const newCoin = JSON.parse(`{"${name}": "${coinType}"}`);
+    const newOutput = [...output, newCoin];
+    if (numIndex <= coinTypes.length - 1) {
+      return coinCounterFunc(coinTypes, coinNames, numIndex + 1, coinOut, newOutput);
+    } else {
+      return (output);
     }
-  }
-  getCoins(0, total);
-  return (output);
 }
 
-console.log(coinCounter(4.99));
-console.log(coinCounterFunc(4.99));
+// console.log(coinCounter(4.99));
+const amount = 4.99;
+const total = amount * 100;
+const emptyCoins = [];
+const coinTypes = [25, 10, 5, 1]
+const coinNames = ["quarters", "dimes", "nickels", "pennies"]
+console.log(coinCounterFunc(coinTypes, coinNames, 0, total, emptyCoins));
 
 exports.coinCounter = coinCounter;
